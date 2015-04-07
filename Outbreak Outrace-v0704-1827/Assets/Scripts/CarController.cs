b1291f5477e96b127741a5f21b36ea973a30048f
Student105029 <105029@student.sae.edu.au>
2015-04-07 18:32:20 +1000

    ProjectCopiedWithVersionNumber
    07/04 - 6:29pm
    
    James made a copy of the master project.
    The project folder was renamed with a version number composed of
    (-v*date*-*time*)
    A new material "toBeDeleted" was added to test making changes to the
    project then syncing to github.

---
+++ Outbreak Outrace-v0704-1827/Assets/Scripts/CarController.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarController : MonoBehaviour {
+
+    public PlayerCar Player;
+	Rigidbody RigBody;
+
+    GameController Game = new GameController();
+
+	// Use this for initialization
+	void Start () {
+        RigBody = GetComponent<Rigidbody>();
+        Player = Game.CreateNewPlayer();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        DoPlayerMovement();
+	
+	}
+
+    void DoPlayerMovement()
+    {
+		if (Input.GetKey(KeyCode.Space)) {
+            
+			Accelerate ();
+
+		} 
+        else 
+        {
+			Deccelerate ();
+		}
+
+		//Player.PlayerPos.x += Player.PlayerSpeed;
+
+		//this.transform.position = Player.PlayerPos;
+
+		///better to use Rigidbody AddForce instead, works better on the ramp (JC);
+
+    }
+
+    void Accelerate()
+    {
+
+        print("Accelerating");
+        Player.PlayerSpeed = Player.PlayerSpeed += Time.deltaTime; 
+
+        RigBody.AddForce(Vector3.right * 20 * Player.PlayerSpeed, ForceMode.Impulse);
+        
+    }
+    
+    void Deccelerate()
+    {
+        if(Player.PlayerSpeed > 0)
+        {
+            print("Decelerating");
+            Player.PlayerSpeed -= Time.deltaTime;
+
+            RigBody.AddForce(Vector3.left * 20 * Player.PlayerSpeed, ForceMode.Acceleration);
+            
+        }
+        
+    }
+
+}
---
+++ Outbreak Outrace-v0704-1827/Assets/Scripts/UI Scripts/UI_Timer.cs
@@ -0,0 +1,165 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UI_Timer : MonoBehaviour {
+
+	public Text Timertext;
+	//public UnityEngine.UI.Text Timertext;
+	public float Currenttime = 0;
+	public bool FinishedPressed = false;
+	public bool PausedPressed = false;
+
+	public Button Finish;
+
+	float multiplier = 1;
+	
+	public Button Pu;
+	public Button Z;
+	public Button H;
+
+	public Button Restart;
+	public Canvas RestartCanvas;
+	public Canvas FinishCanvas;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+		Finish = Finish.GetComponent<Button>();
+		Restart = Restart.GetComponent<Button>();
+		RestartCanvas = RestartCanvas.GetComponent<Canvas>();
+		FinishCanvas = FinishCanvas.GetComponent<Canvas>();
+		
+		Pu = Pu.GetComponent<Button>();
+		Z = Z.GetComponent<Button>();
+		H = H.GetComponent<Button>();
+
+		RestartCanvas.enabled = false;
+	
+
+	}
+
+	public void Pausepress()
+	{
+		PausedPressed = true;
+	}
+
+	public void UnPausepress()
+	{
+		PausedPressed = false;
+	}
+
+	
+	public void FinishPress()
+	{
+		
+		Pu.enabled = false;
+		Z.enabled = false;
+		H.enabled = false;
+		FinishedPressed = true;
+		RestartCanvas.enabled = true;
+		FinishCanvas.enabled = false;
+		
+
+		
+		if(Currenttime <= 10)
+		{
+			multiplier=4;
+		}
+		else if(Currenttime <= 20)
+		{
+			multiplier=3;
+		}
+		else if(Currenttime <= 30)
+		{
+			multiplier=2;
+		} else {multiplier=1;}
+
+
+
+	}
+
+	public bool PressedX ()
+	{
+		return FinishedPressed;
+	}
+
+
+
+	public float ScoremultiplierX ()
+	{
+		return multiplier;
+	}
+
+
+	public void SetValue()
+	{
+		if(!FinishedPressed)
+		{
+		Timertext.text = Currenttime.ToString();
+		}
+	}
+
+
+	
+	// Update is called once per frame
+
+
+	void Update () {
+
+		if(!PausedPressed)
+		{
+			Currenttime += Time.deltaTime;
+		}
+
+		if(FinishedPressed == true)
+		{
+			ScoremultiplierX();
+		}
+
+		SetValue();
+	
+	}
+
+
+
+public void RestartSimulation()
+
+	{
+		Pu.enabled = true;
+		Z.enabled = true;
+		H.enabled = true;
+		FinishedPressed = false;
+		RestartCanvas.enabled = false;
+		FinishCanvas.enabled = true;
+
+		Currenttime = 0;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
