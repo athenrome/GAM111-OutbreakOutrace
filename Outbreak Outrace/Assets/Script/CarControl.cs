c898f956db608899837a5d84eb1d742bed77c633
JChiu <jefone88@hotmail.com>
2015-04-09 21:20:11 +1000

    further Scripts improvement and other stuffs
    Things that doesn't manipulate the car's in Carcontrol.cs have moved to
    the script 'PickupInterraction.cs'.
    
    Makes the car able to travel backward, therefore, half solved the
    deceleration. Makes the car boost forward a little bit while jumping,
    thus enable it to avoid obsticals (in previous version, the jump just a
    simple up and down jump, now is in an semi-arc).
    
    Replaced those ramps with polygon collider with ramps that have edge
    collider, because raycast won't detect anything that got a polygon
    collider with it. (possible bug) Nonetheless, nothing major had changed
    to the track.
    
    Build a Demo so that we have something to shows to the client.

--- Outbreak Outrace/Assets/Script/CarControl.cs
+++ Outbreak Outrace/Assets/Script/CarControl.cs
@@ -2,25 +2,25 @@
 using System.Collections;
 using UnityEngine.UI;
 /// <summary>
-/// This script will assume how joystick works, the varible for that will be controlled by mouseOri and movedMouse
+///
 /// </summary>
 
 
 public class CarControl : MonoBehaviour {
 	public Rigidbody2D carShell; //To control the movment of the car
-	public Text carSta, carHealthDis;
+	public Text carSta;
 	public Transform[] wheels;
-	public float jumpHeight, carHealth, carAr;
+	public float jumpHeight, nitrate;
 	public bool haveSpike;
 	public Camera viewPortManipu;
+	public int niGas;
 
-	private float speed, mouseMovedOff, slowed, score, nitrate, carToGroundDis;
+	private float speed, mouseMovedOff, slowed, score, carToGroundDis;
 	private float displaySpeed;
 	private float acceleration = 100f;
 	//private float deceleration = -50f;
-	private Vector2 mouseOri, movedMouse;
+	//private Vector2 mouseOri, movedMouse;
 	private RaycastHit2D isFloored, AirToGroundRC;
-	private int spikeSpateEndu, niGas;
 	private Vector3 carOriRot;
 
 	// Use this for initialization
@@ -28,12 +28,10 @@ public class CarControl : MonoBehaviour {
 		carToGroundDis = 0;
 		niGas = 1;
 		carOriRot = transform.localEulerAngles;
-		mouseOri = Camera.main.ScreenToViewportPoint (Input.mousePosition);
+		//mouseOri = Camera.main.ScreenToViewportPoint (Input.mousePosition);
 		score = 0;
 		slowed = 1; //affect the car's current speed
 		nitrate = 1;
-		spikeSpateEndu = 0;
-		DisplayHealthScore(carHealth, carAr, score);
 	}
 	
 	// Update is called once per frame
@@ -46,30 +44,46 @@ public class CarControl : MonoBehaviour {
 		speed = (input * acceleration * slowed * nitrate);
 
 		//Check to see if the car is grounded.
-		isFloored = Physics2D.Raycast(transform.position, Vector2.up, -2, 1 << LayerMask.NameToLayer("Road")); 
-		if (isFloored.collider != null) 
+		isFloored = Physics2D.Raycast(transform.position, transform.up, -4, 1 << LayerMask.NameToLayer("Road")); 
+		//Debug.DrawRay (transform.position, transform.up * -4, Color.red);
+		//Debug.Log (isFloored.collider.gameObject);
+		if (isFloored.collider != null)
 		{
 			//Acceleration
-			if (input > 0)
+			if (input != 0)
 			{
 				carShell.AddRelativeForce(Vector2.right * speed, ForceMode2D.Force);
 				//carShell.AddRelativeForce (Vector2.right * input * acceleration * slowed * nitrate, ForceMode2D.Force);
-			}
+			}  
 
 			if (carShell.velocity.magnitude > 0.01) //If the car is grounded and moving 
 				//Fix required. This code checks if the carShell is moving at all. As a result, it will trigger even if the car is falling or airborne.
 			{
 				if(Input.GetMouseButtonDown(0)){
-					carShell.AddRelativeForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);} //makes the car jump 
+					carShell.AddRelativeForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);//makes the car jump 
+					carShell.AddRelativeForce(Vector2.right * speed, ForceMode2D.Impulse); //create an arh jump.
+				} 
 
 				//Deceleration	
 				//TODO: Deceleration may not interact with "slowed" properly. Untested. 
-				if (input < 0)
+				//SOF: current deceleration is the same as reversing.
+				/*if (input < 0)
 				{
-					if(speed > 0){
-						carShell.AddRelativeForce(Vector2.right * speed, ForceMode2D.Force);}
-				}
+					if(speed > 0)
+					{
+						carShell.AddRelativeForce(Vector2.right * speed, ForceMode2D.Force);
+					}
+				}*/			
+			}
+
+			//using the nitrate
+			if (Input.GetMouseButtonDown(1)) {
+				nitrate = niGas;
+			}
 			
+			//creates a graduation of slowing down from using the nitrate
+			if (nitrate > 1) {
+				nitrate -= Time.smoothDeltaTime;
 			}
 		}
 
@@ -80,16 +94,6 @@ public class CarControl : MonoBehaviour {
 			//Illusion for the wheels
 				wheels[0].Rotate(Vector3.forward * -speed, Space.Self);
 				wheels[1].Rotate(Vector3.forward * -speed, Space.Self);
-
-			//using the nitrate
-			if (Input.GetMouseButtonDown(1)) {
-				nitrate = niGas;
-			}
-
-			//creates a graduation of slowing down from using the nitrate
-			if (nitrate > 1) {
-				nitrate -= Time.smoothDeltaTime;
-			}
 		}
 
 
@@ -97,86 +101,10 @@ public class CarControl : MonoBehaviour {
 		displaySpeed = speed; //ATTENTION: Displaying speed is currently broken.
 		//This currently displays the vehicle's acceleration. 
 		carSta.text = displaySpeed.ToString ("F2") + "km/s"; //Displaying the car's speed
-		Debug.Log ("This is the nitrate value: " + nitrate);
+		//Debug.Log ("This is the nitrate value: " + nitrate);
 
 
 		//TODO: Pull the code that isn't related to controlling the car (eg interaction with pickups), out of CarControl.cs and into a different Car script
-		
-
-		//disable the spike
-		if (spikeSpateEndu <= 0) {
-			haveSpike = false;
-		}
-	}
-
-
-	void OnTriggerEnter2D(Collider2D c) {
-		PenaltyRewards (c.GetComponent<Collider2D> ().gameObject.tag);
-	}
-
-	void PenaltyRewards (string hitObTag) {
-		float remainDam = 0; //The remaining damage once absorbed by the armour
-
-		switch (hitObTag) {
-		case "ZombieS": //A single zombie is hit
-			score += 10;
-			DisplayHealthScore(carHealth, carAr, score);
-			break;
-		case "ZombieH": //A horde of zombies is hit
-			if (!haveSpike) { //This block won't happens if the player have equipped a spike.
-				if (carAr > 0) {
-					remainDam = 200 * 0.2f;
-					carAr -= 200 * 0.8f;
-					carHealth -= remainDam; 
-				}
-
-				if (carAr <= 0) { //armour not active
-					carHealth -= 200;
-				}
-
-				slowed = 0.5f;
-			}
-
-			if (haveSpike) {
-				spikeSpateEndu--;
-			}
-
-			score += 50;
-			DisplayHealthScore(carHealth, carAr, score);
-
-			if (carHealth <= 0) {
-				Time.timeScale = 0;
-				carHealthDis.text = "Gameover";
-			}
-
-			break;
-		case "Barricade": //hit a barricade
-			if (!haveSpike) { ////This block won't happens if the player have equipped a spike.
-				slowed = 0.1f;
-			}
-
-			if (haveSpike) {
-				spikeSpateEndu--;
-			}
-			break;
-		case "Backpack":
-			score += 200;
-			DisplayHealthScore(carHealth, carAr, score);
-			break;
-		case "Nitros":
-			niGas++;
-			DisplayHealthScore(carHealth, carAr, score);
-			break;
-		case "Spike":
-			spikeSpateEndu = 10;
-			haveSpike = true;
-			DisplayHealthScore(carHealth, carAr, score);
-			break;
-		case "Armor":
-			carAr = 500;
-			DisplayHealthScore(carHealth, carAr, score);
-			break;
-		}
 	}
 
 	void LateUpdate() {
@@ -184,17 +112,11 @@ public class CarControl : MonoBehaviour {
 		AirToGroundRC = Physics2D.Raycast(transform.position, Vector2.up, -100, 
 		                                  1 << LayerMask.NameToLayer("Road"));
 		carToGroundDis = Vector2.Distance (transform.position, AirToGroundRC.point);
-		Debug.Log (AirToGroundRC.point);
-		viewPortManipu.orthographicSize = carToGroundDis + 10; //More zoomed out when the car is air travelling.
+		viewPortManipu.orthographicSize = carToGroundDis + 20; //More zoomed out when the car is air travelling.
 		
 		//to prevent over zooming
 		if (viewPortManipu.orthographicSize > 25) {
-			viewPortManipu.orthographicSize = 20;
+			viewPortManipu.orthographicSize = 40;
 		}
-	}
-
-	void DisplayHealthScore (float h, float a, float s) {
-		carHealthDis.text = "health: " + h.ToString () + "\n Scrap Metal: " + a.ToString () 
-			+ "\n Exp: " + s.ToString () + "\n SP: " + spikeSpateEndu.ToString () + "\n current Gas meter: " + nitrate.ToString ();
 	}
 }
---
+++ Outbreak Outrace/Assets/Script/PickupInterraction.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PickupInterraction : MonoBehaviour {
+	
+	public Text carHealthDis;
+	public float carAr, carHealth;
+	public bool haveSpike;
+	
+	private float speed, mouseMovedOff, slowed, score, carToGroundDis;
+	//private float deceleration = -50f;
+	private int spikeSpateEndu;
+	private CarControl cc;
+	
+	// Use this for initialization
+	void Start () {
+		cc = GetComponent<CarControl> ();
+		//mouseOri = Camera.main.ScreenToViewportPoint (Input.mousePosition);
+		score = 0;
+		spikeSpateEndu = 0;
+		DisplayHealthScore(carHealth, carAr, score);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//disable the spike
+		if (spikeSpateEndu <= 0) {
+			haveSpike = false;
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D c) {
+		PenaltyRewards (c.GetComponent<Collider2D> ().gameObject.tag);
+	}
+	
+	void PenaltyRewards (string hitObTag) {
+		float remainDam = 0; //The remaining damage once absorbed by the armour
+		
+		switch (hitObTag) {
+		case "ZombieS": //A single zombie is hit
+			score += 10;
+			DisplayHealthScore(carHealth, carAr, score);
+			break;
+		case "ZombieH": //A horde of zombies is hit
+			if (!haveSpike) { //This block won't happens if the player have equipped a spike.
+				if (carAr > 0) {
+					remainDam = 200 * 0.2f;
+					carAr -= 200 * 0.8f;
+					carHealth -= remainDam; 
+				}
+				
+				if (carAr <= 0) { //armour not active
+					carHealth -= 200;
+				}
+				
+				slowed = 0.5f;
+			}
+			
+			if (haveSpike) {
+				spikeSpateEndu--;
+			}
+			
+			score += 50;
+			DisplayHealthScore(carHealth, carAr, score);
+			
+			if (carHealth <= 0) {
+				Time.timeScale = 0;
+				carHealthDis.text = "Gameover";
+			}
+			
+			break;
+		case "Barricade": //hit a barricade
+			if (!haveSpike) { ////This block won't happens if the player have equipped a spike.
+				slowed = 0.1f;
+			}
+			
+			if (haveSpike) {
+				spikeSpateEndu--;
+			}
+			break;
+		case "Backpack":
+			score += 200;
+			DisplayHealthScore(carHealth, carAr, score);
+			break;
+		case "Nitros":
+			cc.niGas++;
+			DisplayHealthScore(carHealth, carAr, score);
+			break;
+		case "Spike":
+			spikeSpateEndu = 10;
+			haveSpike = true;
+			DisplayHealthScore(carHealth, carAr, score);
+			break;
+		case "Armor":
+			carAr = 500;
+			DisplayHealthScore(carHealth, carAr, score);
+			break;
+		}
+	}
+
+	void DisplayHealthScore (float h, float a, float s) {
+		carHealthDis.text = "health: " + h.ToString () + "\n Scrap Metal: " + a.ToString () 
+			+ "\n Exp: " + s.ToString () + "\n SP: " + spikeSpateEndu.ToString () + "\n current Gas meter: " 
+				+ cc.nitrate.ToString ();
+	}
+}
