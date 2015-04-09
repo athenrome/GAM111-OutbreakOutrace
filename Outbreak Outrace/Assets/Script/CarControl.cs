d6aff834d09b6c673e5d0bb23295e042900e6757
Student105029 <105029@student.sae.edu.au>
2015-04-09 14:33:22 +1000

    Updated CarControl.cs (James)
    09/04 - 2:30pm
    
    Changelog:
    Created float acceleration to replace magic number 100.
    Replaced float kms with float displaySpeed.
    Renamed backwheel to carShell due to the fact that the backwheel doesn't
    have a rigidbody but the carShell does.
    
    Implemented controller input.
    Broke the display speed due to redefining 'speed'.

--- Outbreak Outrace/Assets/Script/CarControl.cs
+++ Outbreak Outrace/Assets/Script/CarControl.cs
@@ -7,14 +7,17 @@ using UnityEngine.UI;
 
 
 public class CarControl : MonoBehaviour {
-	public Rigidbody2D backWheel; //To control the movment of the car
+	public Rigidbody2D carShell; //To control the movment of the car
 	public Text carSta, carHealthDis;
 	public Transform[] wheels;
 	public float jumpHeight, carHealth, carAr;
 	public bool haveSpike;
 	public Camera viewPortManipu;
 
-	private float speed, mouseMovedOff, slowed, score, kms, nitrate, carToGrondDis;
+	private float speed, mouseMovedOff, slowed, score, nitrate, carToGroundDis;
+	private float displaySpeed;
+	private float acceleration = 100f;
+	//private float deceleration = -50f;
 	private Vector2 mouseOri, movedMouse;
 	private RaycastHit2D isFloored, AirToGroundRC;
 	private int spikeSpateEndu, niGas;
@@ -22,7 +25,7 @@ public class CarControl : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		carToGrondDis = 0;
+		carToGroundDis = 0;
 		niGas = 1;
 		carOriRot = transform.localEulerAngles;
 		mouseOri = Camera.main.ScreenToViewportPoint (Input.mousePosition);
@@ -35,53 +38,77 @@ public class CarControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		movedMouse = Camera.main.ScreenToViewportPoint (Input.mousePosition);
 
-		if (movedMouse.x != mouseOri.x) {//signified the player has moved the moue
-			if (movedMouse.x > mouseOri.x) {
-				mouseMovedOff = Vector2.Distance(movedMouse, mouseOri); //the offset from the original mouse position.
-				speed = mouseMovedOff * 100;
-				//Illusion for the wheels
+		//read input from controller
+		//also works with keyboard keys leftArrow and rightArrow without additional code
+		var input = Input.GetAxis ("Horizontal");
+		//Debug.Log (input);
+		speed = (input * acceleration * slowed * nitrate);
+
+		//Check to see if the car is grounded.
+		isFloored = Physics2D.Raycast(transform.position, Vector2.up, -2, 1 << LayerMask.NameToLayer("Road")); 
+		if (isFloored.collider != null) 
+		{
+			//Acceleration
+			if (input > 0)
+			{
+				carShell.AddRelativeForce(Vector2.right * speed, ForceMode2D.Force);
+				//carShell.AddRelativeForce (Vector2.right * input * acceleration * slowed * nitrate, ForceMode2D.Force);
+			}
+
+			if (carShell.velocity.magnitude > 0.01) //If the car is grounded and moving 
+				//Fix required. This code checks if the carShell is moving at all. As a result, it will trigger even if the car is falling or airborne.
+			{
+				if(Input.GetMouseButtonDown(0)){
+					carShell.AddRelativeForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);} //makes the car jump 
+
+				//Deceleration	
+				//TODO: Deceleration may not interact with "slowed" properly. Untested. 
+				if (input < 0)
+				{
+					if(speed > 0){
+						carShell.AddRelativeForce(Vector2.right * speed, ForceMode2D.Force);}
+				}
+			
+			}
+		}
+
+		//If the car is moving
+		//Fix required. This code checks if the carShell is moving at all. As a result, it will trigger even if the car is falling or airborne.
+		if (carShell.velocity.magnitude > 0.01) 
+		{
+			//Illusion for the wheels
 				wheels[0].Rotate(Vector3.forward * -speed, Space.Self);
 				wheels[1].Rotate(Vector3.forward * -speed, Space.Self);
 
-				//The car can only jumps when is moving
-				if (Input.GetMouseButtonDown(0)) {
-					isFloored = Physics2D.Raycast(transform.position, Vector2.up, -2, 
-					                            1 << LayerMask.NameToLayer("Road")); //Check to see if the car is grounded.
-
-					if (isFloored.collider != null) {
-						backWheel.AddRelativeForce(Vector2.up * jumpHeight, ForceMode2D.Impulse); //makes the car jump
-					}
-				}
-			}
-
-			if (movedMouse.x < mouseOri.x) {// direction for the brakes
-				speed = 0; 
-				transform.localEulerAngles = carOriRot; //flip it back to how it was 
+			//using the nitrate
+			if (Input.GetMouseButtonDown(1)) {
+				nitrate = niGas;
 			}
 
 			//creates a graduation of slowing down from using the nitrate
 			if (nitrate > 1) {
 				nitrate -= Time.smoothDeltaTime;
 			}
-
-			//using the nitrate
-			if (Input.GetMouseButtonDown(1)) {
-				nitrate = niGas;
-			}
 		}
 
-		backWheel.AddRelativeForce (Vector2.right * speed * slowed * nitrate, ForceMode2D.Force); //moving the car
-		kms = speed * slowed * nitrate;
-		carSta.text = kms.ToString ("F2") + "km/s"; //Displaying the car's status
-		Debug.Log (nitrate);
+
+		
+		displaySpeed = speed; //ATTENTION: Displaying speed is currently broken.
+		//This currently displays the vehicle's acceleration. 
+		carSta.text = displaySpeed.ToString ("F2") + "km/s"; //Displaying the car's speed
+		Debug.Log ("This is the nitrate value: " + nitrate);
+
+
+		//TODO: Pull the code that isn't related to controlling the car (eg interaction with pickups), out of CarControl.cs and into a different Car script
+		
 
 		//disable the spike
 		if (spikeSpateEndu <= 0) {
 			haveSpike = false;
 		}
 	}
+
 
 	void OnTriggerEnter2D(Collider2D c) {
 		PenaltyRewards (c.GetComponent<Collider2D> ().gameObject.tag);
@@ -95,7 +122,7 @@ public class CarControl : MonoBehaviour {
 			score += 10;
 			DisplayHealthScore(carHealth, carAr, score);
 			break;
-		case "ZombieH": //A horge of zombies is hit
+		case "ZombieH": //A horde of zombies is hit
 			if (!haveSpike) { //This block won't happens if the player have equipped a spike.
 				if (carAr > 0) {
 					remainDam = 200 * 0.2f;
@@ -123,7 +150,7 @@ public class CarControl : MonoBehaviour {
 			}
 
 			break;
-		case "Barricade": //hit a barricard
+		case "Barricade": //hit a barricade
 			if (!haveSpike) { ////This block won't happens if the player have equipped a spike.
 				slowed = 0.1f;
 			}
@@ -156,9 +183,9 @@ public class CarControl : MonoBehaviour {
 		//For air to ground calculation
 		AirToGroundRC = Physics2D.Raycast(transform.position, Vector2.up, -100, 
 		                                  1 << LayerMask.NameToLayer("Road"));
-		carToGrondDis = Vector2.Distance (transform.position, AirToGroundRC.point);
+		carToGroundDis = Vector2.Distance (transform.position, AirToGroundRC.point);
 		Debug.Log (AirToGroundRC.point);
-		viewPortManipu.orthographicSize = carToGrondDis + 10; //More zoomed out when the car is air travelling.
+		viewPortManipu.orthographicSize = carToGroundDis + 10; //More zoomed out when the car is air travelling.
 		
 		//to prevent over zooming
 		if (viewPortManipu.orthographicSize > 25) {
